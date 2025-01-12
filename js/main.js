72153ead38c17bfe92ee732295e454a2a55830d6
barakdo <barakdoron3@gmail.com>
2025-01-12 17:28:58 +0200

    product page and styling

--- js/main.js
+++ js/main.js
@@ -16,13 +16,20 @@ const printProducts = (products) => {
   console.log(products);
   let str = "";
   for (const product of products) {
-    str += '<div class="product-card">';
-    str += '<img src="'+ product['imgUrl'] +'" alt="Sapphire Ring">';
+    str += '<div id=product' + product["productId"] + ' class="product-card">';
+    str += '<img src="' + product['imgUrl'] + '" alt="Sapphire Ring">';
     str += '<div class="product-info">';
-    str += '<h3>'+ product['productName'] + '</h3>';
-    str += '<p>'+ product['productPrice'] + '</p>';
+    str += '<h3>' + product['productName'] + '</h3>';
+    str += '<p>' + product['productPrice'] + '</p>';
     str += '</div>';
     str += '</div>';
   }
   document.getElementById("products").innerHTML = str;
+  for (const product of products) {
+    const productCard = document.getElementById("product" + product["productId"]);
+    productCard.addEventListener('click', (event) => {
+      localStorage.setItem('productId', product["productId"]);
+      window.location.replace('/html/product.html');
+    });
+  }
 }
---
+++ js/product.js
@@ -0,0 +1,57 @@
+const getProducts = async () => {
+  try {
+    const response = await fetch('https://6wdws3ku5i.execute-api.us-east-1.amazonaws.com/dev/products');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+window.onload = async () => {
+  printProduct(product(await getProducts()));
+}
+
+const product = (products) => {
+  const productId = localStorage.getItem("productId");
+  for (const product of products) {
+    if (product["productId"] == productId) {
+      return product;
+    }
+  }
+  return null;
+}
+
+const printProduct = (product) => {
+  document.querySelector("link[rel='icon']").href = product["imgUrl"];
+  document.title = product["productName"];
+  let str = "";
+  str += '<div class="product-image">';
+  str += '<img src="' + product["imgUrl"] + '" alt="Sapphire Ring">';
+  str += '</div>';
+  str += '<div class="product-details">';
+  str += '<h2 class="product-name">' + product["productName"] + '</h2>';
+  str += ' <p class="price">' + product["productPrice"] + "$" + '</p>';
+  str += ' <p class="description">' + product["productDescription"] + '</p>';
+  str += '<button class="buy-now-btn">Buy Now</button>';
+  str += '</div>';
+
+  document.getElementById("product").innerHTML = str;
+  colorHandle();
+}
+
+const colorHandle = () => {
+    // Select the product image
+    const productImage = document.querySelector(".product-page img");
+    const productCanvas = document.getElementById("product");
+    // Add event listener for mouseover
+    productCanvas.addEventListener("mouseover", () => {
+      productImage.style.filter = "grayscale(0%)";
+    });
+  
+    // Add event listener for mouseout
+    productCanvas.addEventListener("mouseout", () => {
+      productImage.style.filter = "grayscale(100%)";
+    });
+  
+}
