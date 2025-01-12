7b6541257e004cd73318090f8c633a1a70a5862c
barakdo <barakdoron3@gmail.com>
2025-01-12 16:19:31 +0200

    GET Projucts

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
-const getProducts = () =>{
-  
-  fetch('https://6wdws3ku5i.execute-api.us-east-1.amazonaws.com/dev/products', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then(response => response.json())
-    .then(data => {
-      return data;
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
+const getProducts = async () => {
+  try {
+    const response = await fetch('https://6wdws3ku5i.execute-api.us-east-1.amazonaws.com/dev/products');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
 }
 
-window.onload = () => {
-  const products = getProducts();
+window.onload = async () => {
+  printProducts(await getProducts());
+}
+
+const printProducts = (products) => {
   console.log(products);
+  let str = "";
+  for (const product of products) {
+    str += '<div class="product-card">';
+    str += '<img src="'+ product['imgUrl'] +'" alt="Sapphire Ring">';
+    str += '<div class="product-info">';
+    str += '<h3>'+ product['productName'] + '</h3>';
+    str += '<p>'+ product['productPrice'] + '</p>';
+    str += '</div>';
+    str += '</div>';
+  }
+  document.getElementById("products").innerHTML = str;
 }
